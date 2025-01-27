33ab28343c134c068c3fae8adf59de7cc438353c
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2025-01-28 00:54:37 +0100

    chore: first commit

---
+++ lib/beam_bot_web/live/home_live.ex
@@ -0,0 +1,12 @@
+defmodule BeamBotWeb.HomeLive do
+  use Phoenix.LiveView
+
+  alias BeamBot.Bot
+  alias BeamBot.Bot.State
+
+  def render(assigns) do
+    ~L"""
+    Hello
+    """
+  end
+end
--- lib/beam_bot_web/router.ex
+++ lib/beam_bot_web/router.ex
@@ -17,7 +17,7 @@ defmodule BeamBotWeb.Router do
   scope "/", BeamBotWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", HomeLive
   end
 
   # Other scopes may use custom stacks.
