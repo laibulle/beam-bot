283dc1d8e9bde715a395f690061a26ba29ffbce1
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2025-02-14 17:28:38 +0100

    chore: first commit

--- lib/hex_tate/state_machine.ex
+++ lib/hex_tate/state_machine.ex
@@ -1,6 +1,6 @@
 defmodule HexTate.StateMachine do
   defstruct [:id, :context, :initial, :events, :state]
 
-  def send(state_machine = %__MODULE__{}, event) do
+  def send(_state_machine = %__MODULE__{}, _event) do
   end
 end
