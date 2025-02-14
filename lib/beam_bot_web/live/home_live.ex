aefd44fcf92f671a7ddf0f2c65b1f5a299fcdb98
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2025-02-14 17:35:03 +0100

    chore: first commit

--- lib/beam_bot_web/live/home_live.ex
+++ lib/beam_bot_web/live/home_live.ex
@@ -1,9 +1,31 @@
 defmodule BeamBotWeb.HomeLive do
   use Phoenix.LiveView
 
+  alias BeamBot.Infrastructure.Adapters.BinanceReqAdapter
+
   def render(assigns) do
     ~H"""
-    Hello
+    <div>
+      <%= for account_info <- @account_info["balances"] do %>
+        <%= if account_info["free"] |> parse_value() > 0 do %>
+          <div>
+            {account_info["asset"]}
+            {account_info["free"]}
+            {account_info["locked"]}
+          </div>
+        <% end %>
+      <% end %>
+    </div>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, account_info} = BinanceReqAdapter.get_account_info() |> dbg()
+    {:ok, socket |> assign(account_info: account_info)}
+  end
+
+  defp parse_value(value) do
+    {v, _} = Float.parse(value)
+    v
   end
 end
