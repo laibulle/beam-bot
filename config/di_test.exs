d1e9a19dcb7208c0cb94cd2d4b6c3a7e75690323
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2025-03-27 11:19:39 +0100

    chore: first commit

---
+++ config/di_test.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :beam_bot,
+  trading_pairs_repository:
+    BeamBot.Exchanges.Infrastructure.Adapters.Ecto.TradingPairsRepositoryMock,
+  binance_req_adapter: BeamBot.Exchanges.Infrastructure.Adapters.Exchanges.BinanceReqAdapter
---
+++ test/support/mocks.ex
@@ -0,0 +1,5 @@
+import Mox
+
+defmock(BeamBot.Exchanges.Infrastructure.Adapters.Ecto.TradingPairsRepositoryMock,
+  for: BeamBot.Exchanges.UseCases.Ports.TradingPairsRepository
+)
