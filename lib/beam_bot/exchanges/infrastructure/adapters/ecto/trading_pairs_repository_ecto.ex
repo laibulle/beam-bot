04b1e354c19384ad4121b42034d20ff72ddd85e2
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2025-03-27 10:39:02 +0100

    chore: first commit

--- lib/beam_bot/exchanges/infrastructure/adapters/ecto/trading_pairs_repository_ecto.ex
+++ lib/beam_bot/exchanges/infrastructure/adapters/ecto/trading_pairs_repository_ecto.ex
@@ -2,4 +2,14 @@ defmodule BeamBot.Exchanges.Infrastructure.Adapters.Ecto.TradingPairsRepositoryE
   @moduledoc """
   This module is responsible for managing the trading pairs.
   """
+  @behaviour BeamBot.Exchanges.UseCases.Ports.TradingPairsRepository
+  alias BeamBot.Repo
+
+  @impl true
+  def get_trading_pairs(exchange_identifier) do
+    case Repo.get_by(BeamBot.Exchanges.Domain.Exchange, identifier: exchange_identifier) do
+      nil -> {:error, "Exchange not found"}
+      exchange -> {:ok, exchange.trading_pairs}
+    end
+  end
 end
---
+++ lib/beam_bot/exchanges/use_cases/ports/trading_pairs_repository.ex
@@ -0,0 +1,8 @@
+defmodule BeamBot.Exchanges.UseCases.Ports.TradingPairsRepository do
+  @moduledoc """
+  This module is responsible for managing the trading pairs.
+  """
+  alias BeamBot.Exchanges.Domain.TradingPair
+
+  @callback get_trading_pairs(String.t()) :: {:ok, list(TradingPair.t())} | {:error, String.t()}
+end
