259ea4c8a100074ea4b5620d5c570d5c65b722ce
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-03-31 23:43:40 +0000

    first commit

--- lib/beam_bot/application.ex
+++ lib/beam_bot/application.ex
@@ -20,15 +20,15 @@ defmodule BeamBot.Application do
 
     prod_children = [
       # Start the trading pairs sync worker
-      BeamBot.Exchanges.Infrastructure.Workers.TradingPairsSyncWorker,
+      # BeamBot.Exchanges.Infrastructure.Workers.TradingPairsSyncWorker,
       # Start the small investor strategy worker
       BeamBot.Strategies.Infrastructure.Workers.SmallInvestorStrategyWorker,
       # Start the Telegram messages sync worker
       # BeamBot.Socials.Workers.TelegramMessagesSyncWorker,
       # Start the Registry for WebSocket connections
-      {Registry, keys: :unique, name: BeamBot.Registry},
+      {Registry, keys: :unique, name: BeamBot.Registry}
       # Start the Binance WebSocket supervisor
-      BeamBot.Exchanges.Infrastructure.Workers.BinanceWsSupervisor
+      # BeamBot.Exchanges.Infrastructure.Workers.BinanceWsSupervisor
     ]
 
     children =
