d54ff69e20bded21a2be2b047e4e2635c14e6759
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-03-31 23:19:06 +0000

    first commit

--- lib/beam_bot/application.ex
+++ lib/beam_bot/application.ex
@@ -20,7 +20,7 @@ defmodule BeamBot.Application do
 
     prod_children = [
       # Start the trading pairs sync worker
-      # BeamBot.Exchanges.Infrastructure.Workers.TradingPairsSyncWorker,
+      BeamBot.Exchanges.Infrastructure.Workers.TradingPairsSyncWorker,
       # Start the small investor strategy worker
       BeamBot.Strategies.Infrastructure.Workers.SmallInvestorStrategyWorker,
       # Start the Telegram messages sync worker
