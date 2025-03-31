ded28f631d1b2fc8037bb837804bfe713420bf94
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-03-31 12:48:40 +0000

    first commit

---
+++ lib/beam_bot/strategies/use_cases/find_best_trading_pair_small_investor_use_case.ex
@@ -0,0 +1,9 @@
+defmodule BeamBot.Strategies.UseCases.FindBestTradingPairSmallInvestorUseCase do
+  @moduledoc """
+  Find the best trading pair for a small investor using simulation over all active trading pairs
+  return a list of trading pairs with the best performance
+  """
+  def find_best_trading_pairs_small_investor(params) do
+    params
+  end
+end
