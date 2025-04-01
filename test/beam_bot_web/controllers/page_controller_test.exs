93c08d057248d958b00cf699ab1231239c922e7b
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-01 16:02:37 +0000

    first commit

--- test/beam_bot_web/controllers/page_controller_test.exs
+++ test/beam_bot_web/controllers/page_controller_test.exs
@@ -1,8 +1,6 @@
 defmodule BeamBotWeb.PageControllerTest do
   use BeamBotWeb.ConnCase, async: true
 
-  alias BeamBot.Exchanges.Infrastructure.Adapters.Ecto.TradingPairsRepositoryMock
-
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Go to Dashboard"
