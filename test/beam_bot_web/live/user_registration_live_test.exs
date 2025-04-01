694d112501f6c8173672e1ee0ca9e4cd816411b6
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-01 21:52:02 +0000

    first commit

--- test/beam_bot_web/live/user_registration_live_test.exs
+++ test/beam_bot_web/live/user_registration_live_test.exs
@@ -50,9 +50,7 @@ defmodule BeamBotWeb.UserRegistrationLiveTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
       response = html_response(conn, 200)
-      assert response =~ email
-      assert response =~ "Settings"
-      assert response =~ "Log out"
+      assert response =~ "Go to Dashboard"
     end
 
     test "renders errors for duplicated email", %{conn: conn} do
@@ -77,7 +75,7 @@ defmodule BeamBotWeb.UserRegistrationLiveTest do
 
       {:ok, _login_live, login_html} =
         lv
-        |> element(~s|main a:fl-contains("Log in")|)
+        |> element(~s|a:fl-contains("Log in")|)
         |> render_click()
         |> follow_redirect(conn, ~p"/users/log_in")
 
