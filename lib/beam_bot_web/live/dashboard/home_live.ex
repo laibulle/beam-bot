25a8d7695a1811ee4782d2787989c9948c281158
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-01 13:38:13 +0000

    first commit

--- lib/beam_bot_web/live/dashboard/home_live.ex
+++ lib/beam_bot_web/live/dashboard/home_live.ex
@@ -102,12 +102,6 @@ defmodule BeamBotWeb.HomeLive do
                     >
                       View Details
                     </.link>
-                    <.link
-                      navigate={~p"/exchange/strategy/#{symbol.symbol}"}
-                      class="block w-full text-center bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                    >
-                      Analyze Strategy
-                    </.link>
                   </div>
                 </div>
               </div>
