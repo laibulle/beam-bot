4396a41be51957372206c88bc44b7b371c0bb921
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-03 08:13:10 +0000

    first commit

---
+++ lib/beam_bot/exchanges/domain/platform_credentials.ex
@@ -0,0 +1,36 @@
+defmodule BeamBot.Exchanges.Domain.PlatformCredentials do
+  @moduledoc """
+  This module is responsible for managing platform credentials for exchanges.
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @type t :: %__MODULE__{
+          id: integer() | nil,
+          api_key: String.t() | nil,
+          api_secret: String.t() | nil,
+          exchange_id: integer() | nil,
+          is_active: boolean(),
+          inserted_at: DateTime.t() | nil,
+          updated_at: DateTime.t() | nil
+        }
+
+  schema "platform_credentials" do
+    field :api_key, :string
+    field :api_secret, :string
+    field :is_active, :boolean, default: true
+
+    belongs_to :exchange, BeamBot.Exchanges.Domain.Exchange
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(platform_credentials, attrs) do
+    platform_credentials
+    |> cast(attrs, [:api_key, :api_secret, :is_active, :exchange_id])
+    |> validate_required([:api_key, :api_secret, :exchange_id])
+    |> foreign_key_constraint(:exchange_id)
+  end
+end
