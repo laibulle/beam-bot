da2299a896312b59305ee8fa1c6a1fccaa0e1848
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-05 19:35:41 +0000

    first commit

--- lib/beam_bot/release.ex
+++ lib/beam_bot/release.ex
@@ -18,6 +18,31 @@ defmodule BeamBot.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def seed do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &run_seeds_for/1)
+    end
+  end
+
+  defp run_seeds_for(repo) do
+    seed_script = priv_path_for(repo, "seeds.exs")
+
+    if File.exists?(seed_script) do
+      Code.eval_file(seed_script)
+    else
+      {:error, "Seeds file not found for #{inspect(repo)}"}
+    end
+  end
+
+  defp priv_path_for(repo, filename) do
+    app = Keyword.get(repo.config(), :otp_app)
+    priv_dir = "#{:code.priv_dir(app)}"
+
+    Path.join([priv_dir, "repo", filename])
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
