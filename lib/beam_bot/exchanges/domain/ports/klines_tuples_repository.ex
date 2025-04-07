ed16118aa2c1f65329b383b3c1265d0751091a44
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-07 19:24:19 +0000

    first commit

--- lib/beam_bot/exchanges/domain/ports/klines_tuples_repository.ex
+++ lib/beam_bot/exchanges/domain/ports/klines_tuples_repository.ex
@@ -4,28 +4,7 @@ defmodule BeamBot.Exchanges.Domain.Ports.KlinesTuplesRepository do
   This is a more memory-efficient alternative to the standard KlinesRepository.
   """
 
-  @type kline_tuple :: {
-          String.t(),
-          String.t(),
-          String.t(),
-          DateTime.t(),
-          Decimal.t(),
-          Decimal.t(),
-          Decimal.t(),
-          Decimal.t(),
-          Decimal.t(),
-          Decimal.t() | nil,
-          integer() | nil,
-          Decimal.t() | nil,
-          Decimal.t() | nil,
-          Decimal.t() | nil
-        }
-
-  @doc """
-  Stores a list of kline tuples in the database.
-  Returns {:ok, count} on success or {:error, reason} on failure.
-  """
-  @callback store_klines_tuples([kline_tuple()]) :: {:ok, integer()} | {:error, String.t()}
+  @type kline_tuple :: list()
 
   @doc """
   Retrieves kline tuples for a given symbol and interval.
@@ -46,14 +25,4 @@ defmodule BeamBot.Exchanges.Domain.Ports.KlinesTuplesRepository do
               symbol :: String.t(),
               interval :: String.t()
             ) :: {:ok, kline_tuple() | nil} | {:error, String.t()}
-
-  @doc """
-  Deletes kline tuples for a given symbol and interval within a time range.
-  """
-  @callback delete_klines_tuples(
-              symbol :: String.t(),
-              interval :: String.t(),
-              start_time :: DateTime.t(),
-              end_time :: DateTime.t()
-            ) :: {:ok, integer()} | {:error, String.t()}
 end
