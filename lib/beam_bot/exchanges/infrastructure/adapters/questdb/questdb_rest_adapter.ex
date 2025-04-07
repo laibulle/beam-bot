827fc98086cbd9ebd9cf23e0f0b7035b9b947520
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-07 19:19:09 +0000

    first commit

--- lib/beam_bot/exchanges/infrastructure/adapters/questdb/questdb_rest_adapter.ex
+++ lib/beam_bot/exchanges/infrastructure/adapters/questdb/questdb_rest_adapter.ex
@@ -16,7 +16,7 @@ defmodule BeamBot.Exchanges.Infrastructure.Adapters.QuestDB.QuestDBRestAdapter d
 
   ## Examples
       iex> BeamBot.Exchanges.Infrastructure.Adapters.QuestDB.QuestDBRestAdapter.get_klines("BTCUSDT", "1h")
-      {:ok, ["BTCUSDT", "1h", "binance", 102642.0, 103800.0, 102504.0, 102777.99, 2215.49639, 228556325.2789463, 1214.18516, 125284750.5204112, 531227.0]}
+      {:ok, [["BTCUSDT", "1h", "binance", 102642.0, 103800.0, 102504.0, 102777.99, 2215.49639, 228556325.2789463, 1214.18516, 125284750.5204112, 531227.0]]}
   """
   def get_klines(symbol, interval, limit \\ 500, start_time \\ nil, end_time \\ nil) do
     query = build_query(symbol, interval, limit, start_time, end_time)
