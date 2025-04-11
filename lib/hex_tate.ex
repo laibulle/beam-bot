979c6be45b3df8379bb7b7d6b4cfe85f3b7f8b9d
Guillaiume Bailleul <guillaume.bailleul@gmail.com>
2025-04-11 22:12:21 +0000

    first commit

--- lib/hex_tate.ex
+++
@@ -1,5 +0,0 @@
-defmodule HexTate do
-  @moduledoc """
-  This module is responsible for managing the HexTate.
-  """
-end
--- lib/hex_tate/state_machine.ex
+++
@@ -1,9 +0,0 @@
-defmodule HexTate.StateMachine do
-  @moduledoc """
-  This module is responsible for managing the state machine.
-  """
-  defstruct [:id, :context, :initial, :events, :state]
-
-  def send(%__MODULE__{} = _state_machine, _event) do
-  end
-end
